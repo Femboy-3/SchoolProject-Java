f9f87ec6b19cdb8bd2694e7c6c0a470e5a2333c4
Femboy-3 <denis.jakob12345@gmail.com>
2025-03-05 09:26:05 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame("Application");
+        window.setSize(500, 300);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLayout(null);
+
+        JButton button = new JButton("Click Me");
+        button.setBounds(100, 70, 100, 30);
+        button.setBackground(new Color(0, 255, 0));
+        button.addActionListener(e -> Print("Nikola Marnikovic"));
+        window.add(button);
+
+        window.setVisible(true);
+    }
+
+    public static void Print(String msg){
+        System.out.println(msg);
+        System.out.println(msg);
+    }
+}
